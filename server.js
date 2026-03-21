a0a0b36fb198ad230b1f68568eb1a07ba9d5895a
Ankit Kumar Pathak <ankit1332005@gmail.com>
2026-03-21 22:51:36 +0530

    Production fixes

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 1335;
 
+app.get('/', (req, res) => {
+  res.send('Fino_X_Change API is running...');
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}...`);
 });
